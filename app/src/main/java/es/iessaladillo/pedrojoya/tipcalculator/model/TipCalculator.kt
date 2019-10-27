b225e9afedd25c9a7c471fbe51ea95d262b3db73
Pablo Romero Montero <pabloromeromontero23@gmail.com>
2019-10-27 22:02:37 +0100

    sin control de errores y sin test

--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
@@ -1,24 +1,45 @@
 package es.iessaladillo.pedrojoya.tipcalculator.model
 
+import java.lang.Math.ceil
+
 
 // TipCalculator class. Its constructor receives bill, percentage and diners
 
-class TipCalculator {
+class TipCalculator(bill: Float, percentage: Float, diners: Int) {
+    val bill: Float
+    val percentage: Float
+    val diners: Int
 
-    fun calculateTip(): Float {
-        // TODO
+    init {
+        this.bill = bill
+        this.percentage = percentage
+        this.diners = diners
+    }
+
+    fun calculateTip(): Float{
+       return (bill * (percentage / 100))
     }
 
     fun calculateTotal(): Float {
-        // TODO
+        return bill + calculateTip()
     }
+
 
     fun calculatePerDiner(): Float {
-        // TODO
+        if (diners != 0)
+            return calculateTotal() / diners
+        else
+            return 0f
+
     }
 
-    fun calculatePerDinerRounded(): Float {
-        // TODO
+    fun calculatePerDinerRounded(): Float{
+        if (diners != 0){
+            return ceil(calculatePerDiner().toDouble()).toFloat()
+        }else{
+            return 0f
+        }
     }
+
 
 }
--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/ui/main/MainActivity.kt
@@ -1,9 +1,95 @@
 package es.iessaladillo.pedrojoya.tipcalculator.ui.main
 
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import es.iessaladillo.pedrojoya.tipcalculator.R
+import es.iessaladillo.pedrojoya.tipcalculator.model.TipCalculator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initsViews()
+    }
 
+    private fun initsViews() {
+        txtBill.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateAll(true)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+        txtDiners.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateAll(false)
+            }
+
+        })
+        txtPercentage.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateAll(true)
+            }
+        })
+
+        btnResetTip.setOnClickListener {reset(true)}
+        btnResetDiners.setOnClickListener {reset(false)}
+    }
+
+    private fun calculateAll(b: Boolean) {
+
+
+        val tipCalculator = TipCalculator(
+            txtBill.text.toString().toFloat(),
+            txtPercentage.text.toString().toFloat(),
+            txtDiners.text.toString().toInt()
+        )
+
+        if (b){
+            txtTip.setText(String.format("%.2f",tipCalculator.calculateTip()))
+            txtTotal.setText(String.format("%.2f",tipCalculator.calculateTotal()))
+            txtPerDiner.setText(String.format("%.2f",tipCalculator.calculatePerDiner()))
+            txtPerDinerRounded.setText(String.format("%.2f",tipCalculator.calculatePerDinerRounded()))
+        }else{
+            txtPerDiner.setText(String.format("%.2f",tipCalculator.calculatePerDiner()))
+            txtPerDinerRounded.setText(String.format("%.2f",tipCalculator.calculatePerDinerRounded()))
+        }
+    }
+
+
+    private fun reset(b: Boolean) {
+        if (b){
+            txtBill.setText(R.string.normalValueEditText)
+            txtPercentage.setText(R.string.percentage_defect_tip)
+        }else{
+            txtDiners.setText(R.string.number_diners)
+            txtPerDiner.setText(R.string.normalValueEditText)
+            txtPerDinerRounded.setText(R.string.number_diners)
+        }
+
+    }
 }
